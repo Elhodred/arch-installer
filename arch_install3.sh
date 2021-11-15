fb1b451e697f71d86c9ecfdfa759a27ae3eae510
Alfonso Pinto <alfonso.pinto@gmail.com>
2021-11-15 23:49:05 +0100

    Stop using bare repo for dotfiles

--- arch_install3.sh
+++ arch_install3.sh
@@ -19,9 +19,8 @@ paru -S picom-jonaburg-git \
 systemctl --user enable pulseaudio
 
 # Get dotfiles
-git clone --separate-git-dir=$HOME/.dotfiles https://github.com/elhodred/dotfiles.git tmpdotfiles
-rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
-rm -r tmpdotfiles
+git clone https://github.com/elhodred/dotfiles.git .dotfiles
+cd .dotfiles
+./install
 
-dots config --local status.showUntrackedFiles no
 xmonad --recompile
